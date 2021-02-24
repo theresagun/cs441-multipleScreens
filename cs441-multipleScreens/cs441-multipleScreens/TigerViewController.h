738f271affd1e01227ab05af8397a6dcc9690f8d
Theresa Gundel <tgundel1@binghamton.edu>
2021-02-24 10:39:05 -0500

    basic functionality done

---
+++ cs441-multipleScreens/cs441-multipleScreens/TigerViewController.h
@@ -0,0 +1,16 @@
+//
+//  TigerViewController.h
+//  cs441-multipleScreens
+//
+//  Created by Theresa Gundel on 2/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TigerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
